18d8677817921ca5b5dfe1d4ee82dd1aa5a21eb3
Shivaibhav <shivaibhavd@apra.in>
2025-01-14 13:41:26 +0530

    updated app header to get notifier

--- include/app.h
+++ include/app.h
@@ -1,11 +1,14 @@
 #pragma once
 #include "timer.h"
+#include "notifier.h"
+#include <memory>
 
 class App
 {
 private:
     Timer timer;
     bool running;
+    std::unique_ptr<Notifier> notifier;
 
     void displayTime(int seconds) const;
 
