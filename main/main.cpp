97c639472c96b5fbd97b0a4cf802d0db40e0a03f
Shivaibhav <shivaibhavd@apra.in>
2025-01-15 16:42:16 +0530

    added error handling in main file

--- main/main.cpp
+++ main/main.cpp
@@ -1,8 +1,18 @@
 #include "../include/app.h"
+#include <iostream>
 
 int main()
 {
-    App app;
-    app.run(); // feedback_15Jan: no error handling here
+    try
+    {
+        App app;
+        app.run();
+    }
+    catch (const std::exception &e)
+    {
+        std::cout << "An error occurred: " << e.what() << std::endl;
+        return 1; // indicates failure
+    }
+
     return 0;
 }
