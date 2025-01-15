90a361cbac319b0066458f9192c419ae02bc7a5f
Shivaibhav <shivaibhavd@apra.in>
2025-01-15 18:54:23 +0530

    updated app header based on feedback

--- include/app.h
+++ include/app.h
@@ -8,12 +8,12 @@ class App
 private:
     Timer timer;
     bool running;
-    std::unique_ptr<Notifier> notifier; // feedback_15Jan: why unique_ptr, put a comment
+    std::unique_ptr<INotifier> notifier; // unique_ptr to ensure single ownership and automatic cleanup
     
 
-    void displayTime(int seconds) const; // feedback_15Jan: good, put the rationale of the const in comment
+    void displayTime(int seconds) const; // const as it does not modify any member variables of the class
 
 public:
     App(); 
-    void run(); // feedback_15Jan: how would you know if run failed + ideally run should be noexcept 
+    bool run() noexcept; // noexcept as it should not throw exceptions
 };
